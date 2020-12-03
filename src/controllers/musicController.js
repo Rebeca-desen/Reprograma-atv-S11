205b0a524dc74f73ed812d8878d8a472e096aeb9
Rebeca-desen <rebecarss01@gmail.com>
2020-12-03 01:44:08 -0300

    atividade concluida

--- src/controllers/musicController.js
+++ src/controllers/musicController.js
@@ -17,8 +17,8 @@ return res.status(200).send(musicFound)
 }
 
 const create = (req, res) => {
-    const {id, title ,duration ,launchYear,favorite, artists} = req.body
-    music.push({id, title ,duration ,launchYear,favorite, artists})
+    const {id, title ,duration ,launchYear,favorited, artists} = req.body
+    music.push({id, title ,duration ,launchYear,favorited, artists})
     fs.writeFile('./src/model/music.json', JSON.stringify(music), 'utf8', function (err){
   if(err){
     return res.status(500).send('algo errado não está certo')
@@ -51,7 +51,7 @@ const updateMusic = (req, res) => {
 
     const deleteMusic = (req, res) => {
         const musicId = req.params.id
-        const musicFound = music.find(movie => movie.id == musicId) 
+        const musicFound = music.find(musica => musica.id == musicId) 
         const musicIndex = music.indexOf(musicFound) 
 
         if (musicIndex >= 0) { 
@@ -70,10 +70,36 @@ const updateMusic = (req, res) => {
 
     }
 
+    const updateFavorite = (req, res) => {
+        const musicId = req.params.id 
+        const favorited = req.body.favorited 
+
+        const musicToUpdate = music.find(musica => musica.id == musicId) 
+        const musicIndex = music.indexOf(musicToUpdate) 
+
+        if (musicIndex >= 0) { 
+            musicToUpdate.favorited = favorited 
+            music.splice(musicIndex, 1, musicToUpdate) 
+        } else {
+            res.status(404).send('achei não')
+        }
+
+        fs.writeFile("./src/model/music.json", JSON.stringify(music), 'utf8', function (err) { 
+            if (err) {
+                res.status(500).send('deu ruim')
+            } else {
+                console.log('tudo nos conformes')
+                const musicaUpdated = music.find((musica) => musica.id == musicId) 
+                res.status(200).send(musicaUpdated) 
+            }
+        })
+    }
+
 module.exports = {
     getAll,
     getById,
     create,
     updateMusic,
-    deleteMusic
+    deleteMusic,
+    updateFavorite
 }
--- src/routes/musicRoute.js
+++ src/routes/musicRoute.js
@@ -7,6 +7,7 @@ router.get('/:id', controller.getById)
 router.post('/', controller.create)
 router.put('/:id', controller.updateMusic)
 router.delete('/:id', controller.deleteMusic)
+router.patch('/:id/favorited', controller.updateFavorite)
 
 
 
