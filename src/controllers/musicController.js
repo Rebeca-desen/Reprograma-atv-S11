e7d8fa1812547ba3e5c61fe328e06369156d3375
Rebeca-desen <rebecarss01@gmail.com>
2020-12-02 23:37:16 -0300

    getById okay

--- src/controllers/musicController.js
+++ src/controllers/musicController.js
@@ -6,6 +6,17 @@ const getAll = (req, res) => {
     res.status(200).send(music)
 }
 
+const getById = (req, res) => {
+    const id = req.params.id
+ 
+const musicFound = music.find((musica) => musica.id == id) 
+if(!musicFound){
+    return res.status(500).send('erro aqui quiridan')
+}
+return res.status(200).send(musicFound)
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getById
 }
